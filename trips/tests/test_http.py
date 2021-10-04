fd210fa591edf7e3ada179d8e9ac74bc1a737de4
root <root@localhost.localdomain>
2021-10-04 07:25:24 -0400

    UI Support

--- trips/tests/test_http.py
+++ trips/tests/test_http.py
@@ -3,6 +3,7 @@ import json
 from inspect import signature
 from re import U
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 from rest_framework import status
 from rest_framework.reverse import reverse
@@ -15,23 +16,22 @@ from trips.models import Trip
 USERNAME = 'user@example.com'
 PASSWORD = 'pAssw0rd!'
 
-def create_user(username=USERNAME, password=PASSWORD): 
-    return get_user_model().objects.create_user(
-        username=username,
-        first_name='Test',
-        last_name='User',
-        password=password
-    )
-
+def create_user(username=USERNAME, password=PASSWORD, group_name='rider'): 
+    group, _ = Group.objects.get_or_create(name=group_name)
+    user = get_user_model().objects.create_user(username=username, password=password)
+    user.groups.add(group)
+    user.save()
+    return user
 
 class AuthenticationTest(APITestCase):
-    def test_user_can_sig_up(self):
+    def test_user_can_sign_up(self):
         response = self.client.post(reverse('sign_up'), data={
             'username': USERNAME,
             'first_name': 'Test',
             'last_name': 'User',
             'password1': PASSWORD,
             'password2': PASSWORD,
+            'group': 'rider'
         })
 
         user = get_user_model().objects.last()                  # 테스트 서버에 등록된 가장 최신 데이터 하나를 가져옴
@@ -40,6 +40,8 @@ class AuthenticationTest(APITestCase):
         self.assertEqual(response.data['username'], user.username)
         self.assertEqual(response.data['first_name'], user.first_name)
         self.assertEqual(response.data['last_name'], user.last_name)
+        self.assertEqual(response.data['group'], user.group)
+
 
     def test_user_can_log_in(self):
         user = create_user()
@@ -64,43 +66,27 @@ class AuthenticationTest(APITestCase):
         
 class HttpTripTest(APITestCase):
     def setUp(self):
-        get_user_model().objects.create_user(
-        username='test22',
-        first_name='Test',
-        last_name='User',
-        password='password22'
-    )
+        self.user = create_user() # changed
+        self.client.login(username=self.user.username, password=PASSWORD)  # changed
 
-        response = self.client.post(reverse('log_in'), data={
-            'username': 'test22',
-            'password': 'password22'
-        })
-
-        self.access = response.data['access']
-
-
-        response = self.client.get(reverse('trips:trip_list'),
-            HTTP_AUTHORIZATION=f'Bearer {self.access}'
-        )
-
-    def test_user_can_list_trips(self):
-        
+    def test_user_can_list_trips(self): # changed
         trips = [
-            Trip.objects.create(pick_up_address='A', drop_off_address='B'),
-            Trip.objects.create(pick_up_address='B', drop_off_address='C'),
+            Trip.objects.create(
+                pick_up_address='A', drop_off_address='B', rider=self.user),
+            Trip.objects.create(
+                pick_up_address='B', drop_off_address='C', rider=self.user),
+            Trip.objects.create(
+                pick_up_address='C', drop_off_address='D')
         ]
-        response = self.client.get(reverse('trips:trip_list'),
-            HTTP_AUTHORIZATION=f'Bearer {self.access}'
-        )
+        response = self.client.get(reverse('trips:trip_list'))
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        exp_trip_ids = [str(trip.id) for trip in trips]
+        exp_trip_ids = [str(trip.id) for trip in trips[0:2]]
         act_trip_ids = [trip.get('id') for trip in response.data]
-        self.assertCountEqual(exp_trip_ids, act_trip_ids)
+        self.assertCountEqual(act_trip_ids, exp_trip_ids)
 
-    def test_user_can_retrieve_trip_by_id(self):
-        trip = Trip.objects.create(pick_up_address='A', drop_off_address='B')
-        response = self.client.get(trip.get_absolute_url(),
-            HTTP_AUTHORIZATION=f'Bearer {self.access}'
-        )
+    def test_user_can_retrieve_trip_by_id(self): # changed
+        trip = Trip.objects.create(
+            pick_up_address='A', drop_off_address='B', rider=self.user)
+        response = self.client.get(trip.get_absolute_url())
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(str(trip.id), response.data.get('id'))
